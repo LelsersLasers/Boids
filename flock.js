d1cd02dc23f9eaad0720c6f90526dcd6f05e5785
Lord-Lelsers <48894200+LelsersLasers@users.noreply.github.com>
2023-06-08 12:22:58 -0700

    Spatial Hashing (is it better than normal?)

--- flock.js
+++ flock.js
@@ -5,7 +5,53 @@ class Flock {
         for (let i = 0; i < BoidSettings.numBoids; i++) {
             this.addBoid();
         }
-	}
+
+        this.boidsSpatial = [];
+        this.boidsSpatial.length = SPATIAL_GRID_COUNT * SPATIAL_GRID_COUNT;
+
+        for (let i = 0; i < this.boidsSpatial.length; i++) {
+            this.boidsSpatial[i] = [];
+        }
+
+        for (let i = 0; i < this.boids.length; i++) {
+            const boid = this.boids[i];
+            this.addBoidToSpatial(boid);
+        }
+    }
+    
+    resetSpatial() {
+        for (let i = 0; i < this.boidsSpatial.length; i++) {
+            this.boidsSpatial[i].length = 0;
+        }
+    }
+
+    addBoidToSpatial(boid) {
+        const index = HASH(boid.pos.mod(Vector.one()));
+        this.boidsSpatial[index].push(boid);
+    }
+
+    getBoidsInRadius(pos, radius) {
+        const boids = [];
+        pos = pos.mod(Vector.one());
+
+        const radiusVec = Vector.one().mul(radius);
+        const spatialVec = Vector.one().mul(SPATIAL_GRID_SIZE);
+
+        const min = pos.sub(radiusVec);
+        const max = pos.add(radiusVec).add(spatialVec);
+
+        for (let x = min.x; x < max.x; x += SPATIAL_GRID_SIZE) {
+            for (let y = min.y; y < max.y; y += SPATIAL_GRID_SIZE) {
+                const gridPos = new Vector(x, y).mod(Vector.one());
+                const index = HASH(gridPos);
+                const spatialBoids = this.boidsSpatial[index];
+
+                boids.push(...spatialBoids);
+            }
+        }
+
+        return boids;
+    }
 
 	update(delta, obstacles) {
 		for (let i = 0; i < this.boids.length; i++) {
@@ -13,9 +59,12 @@ class Flock {
 			boid.calculateBehavior(this, obstacles);
 		}
 
+        this.resetSpatial();
+
         for (let i = 0; i < this.boids.length; i++) {
             const boid = this.boids[i];
             boid.update(delta);
+            this.addBoidToSpatial(boid);
         }
 	}
 
@@ -45,6 +94,12 @@ class Flock {
             }
         } else {
             this.boids.length = BoidSettings.numBoids;
+        }
+
+        this.resetSpatial();
+        for (let i = 0; i < this.boids.length; i++) {
+            const boid = this.boids[i];
+            this.addBoidToSpatial(boid);
         }
     }
 }
